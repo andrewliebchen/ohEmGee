67e3a663ac3232641082b2cae9b568d097e187eb
Andrew Liebchen <andrewliebchen@gmail.com>
2016-06-30 11:28:14 -0700

    Footer

--- src/components/Main.js
+++ src/components/Main.js
@@ -3,12 +3,16 @@ import DocumentTitle from 'react-document-title';
 import { Link } from 'react-router';
 import styles from '../styles/Main.scss';
 
+const avatar = require('../assets/images/andrew.png');
+
 export default class App extends Component {
   render() {
     return (
       <DocumentTitle title="OhEmGee!">
         <div className={styles.container}>
-          <h1 className={styles.title}><strong>OhEmGee</strong> is a collection of experiments for fun and profit</h1>
+          <h1 className={styles.title}>
+            <strong>OhEmGee</strong> is a collection of experiments for fun and profit
+          </h1>
           <nav className={styles.nav}>
             <Link to="simDesk" className={styles.navItem}>
               <span className={styles.icon}>💻</span>
@@ -19,6 +23,15 @@ export default class App extends Component {
               <strong>Cube</strong> three-dee cubes in CSS
             </Link>
           </nav>
+          <footer className={styles.footer}>
+            <div className={styles.avatar}>
+              <img src={avatar}/>
+            </div>
+            <div className={styles.bio}>
+              All this stuff is made by me, <a href="http://andrewliebchen.com">Andrew Liebchen</a>.
+              The source is available on <a href="https://github.com/andrewliebchen/ohemgee">Github</a>, if you're interested.
+            </div>
+          </footer>
         </div>
       </DocumentTitle>
     );
