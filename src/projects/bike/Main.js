791e2cb4334e568c9bf45b90057ff9215cbded78
Andrew Liebchen <andrewliebchen@gmail.com>
2016-07-18 13:52:33 -0700

    Implement and display info

--- src/projects/bike/Main.js
+++ src/projects/bike/Main.js
@@ -1,24 +1,44 @@
 import React, { Component } from 'react';
 import gearsize from 'gear-size-calculator';
+import _ from 'lodash';
 
-// # vars and require
-const calculator = new gearsize.Calculator();
-
-// # fill out some params
-const chainRings = [48];
-const cassette = [16];
-const rimDiameter = 700;
-const tyreSize = 28;
-
-// # generate some gear size data
-const result = calculator.generateGearSizesInInches(chainRings, cassette, rimDiameter, tyreSize);
-
-console.log(result);
 
 export default class Main extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      chainRings: [48],
+      cassette: [16],
+      rimDiameter: 700,
+      tireSize: 28,
+    }
+  }
+
   render() {
+    // const { chainRings, cassette, rimDiameter, tireSize } = this.state;
+    const gearInfo = _.flatten(this._generateGearSizes())[0];
+    console.log(gearInfo);
     return (
-      <div>Bike!</div>
+      <div>
+        <h1>Bike!</h1>
+        <ul>
+          <li>Chain ring: {gearInfo.chainRing}</li>
+          <li>Sprocket: {gearInfo.sprocket}</li>
+          <li>Gear ratio: {gearInfo.details.ratio}</li>
+          <li>Rim and tire diameter: {gearInfo.details.rimAndTyreDiameterInInches} in.</li>
+          <li>
+            <strong>Gear size: {gearInfo.gearSize} in.</strong>
+          </li>
+        </ul>
+      </div>
     );
+  }
+
+  _generateGearSizes = () => {
+    const { chainRings, cassette, rimDiameter, tireSize } = this.state;
+    const calculator = new gearsize.Calculator();
+
+    const result = calculator.generateGearSizesInInches(chainRings, cassette, rimDiameter, tireSize);
+    return result;
   }
 }
