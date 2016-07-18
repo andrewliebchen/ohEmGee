f1adc7b3c5794989c578477242c284f6780b3f64
Andrew Liebchen <andrewliebchen@gmail.com>
2016-07-17 17:58:41 -0700

    Basic calcuation

--- src/projects/bike/Main.js
+++ src/projects/bike/Main.js
@@ -1,4 +1,19 @@
 import React, { Component } from 'react';
+import gearsize from 'gear-size-calculator';
+
+// # vars and require
+const calculator = new gearsize.Calculator();
+
+// # fill out some params
+const chainRings = [48];
+const cassette = [16];
+const rimDiameter = 700;
+const tyreSize = 28;
+
+// # generate some gear size data
+const result = calculator.generateGearSizesInInches(chainRings, cassette, rimDiameter, tyreSize);
+
+console.log(result);
 
 export default class Main extends Component {
   render() {
