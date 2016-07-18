f9ea7f618fb1d84e58a3e0dd376d9c9e9589d787
Andrew Liebchen <andrewliebchen@gmail.com>
2016-07-18 14:41:05 -0700

    Sprocket/chain ring diagram

--- src/projects/bike/Main.js
+++ src/projects/bike/Main.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
 import gearsize from 'gear-size-calculator';
 import _ from 'lodash';
+import Cog from './components/Cog';
 
+const calculator = new gearsize.Calculator();
 
 export default class Main extends Component {
   constructor(props) {
@@ -15,30 +17,80 @@ export default class Main extends Component {
   }
 
   render() {
-    // const { chainRings, cassette, rimDiameter, tireSize } = this.state;
+    const { chainRings, cassette, rimDiameter, tireSize } = this.state;
     const gearInfo = _.flatten(this._generateGearSizes())[0];
-    console.log(gearInfo);
     return (
       <div>
         <h1>Bike!</h1>
+        <div>
+          <label>Chain ring</label>
+          <input
+            type="text"
+            defaultValue={chainRings}
+            onChange={this.handleUpdate.bind(null, 'chainRing')}/>
+        </div>
+        <div>
+          <label>Sprocket</label>
+          <input
+            type="text"
+            defaultValue={cassette}
+            onChange={this.handleUpdate.bind(null, 'sprocket')}/>
+        </div>
+        <div>
+          <label>Rim diameter</label>
+          <input
+            type="text"
+            defaultValue={rimDiameter}
+            onChange={this.handleUpdate.bind(null, 'rimDiameter')}/>
+        </div>
+        <div>
+          <label>Tire size</label>
+          <input
+            type="text"
+            defaultValue={tireSize}
+            onChange={this.handleUpdate.bind(null, 'tireSize')}/>
+        </div>
+
         <ul>
-          <li>Chain ring: {gearInfo.chainRing}</li>
-          <li>Sprocket: {gearInfo.sprocket}</li>
           <li>Gear ratio: {gearInfo.details.ratio}</li>
           <li>Rim and tire diameter: {gearInfo.details.rimAndTyreDiameterInInches} in.</li>
           <li>
             <strong>Gear size: {gearInfo.gearSize} in.</strong>
           </li>
         </ul>
+
+        <Cog
+          teeth={chainRings}
+          size={256}
+          className="chainRing"/>
+        <Cog
+          teeth={cassette}
+          size={84}
+          d2={0.85}
+          className="sprocket"/>
       </div>
     );
   }
 
   _generateGearSizes = () => {
     const { chainRings, cassette, rimDiameter, tireSize } = this.state;
-    const calculator = new gearsize.Calculator();
-
     const result = calculator.generateGearSizesInInches(chainRings, cassette, rimDiameter, tireSize);
     return result;
+  }
+
+  handleUpdate = (component, event) => {
+    const value = event.target.value;
+    switch (component) {
+      case 'chainRing':
+        this.setState({chainRings: [value]});
+      case 'sprocket':
+        this.setState({cassette: [value]});
+      case 'rimDiameter':
+        this.setState({rimDiameter: value});
+      case 'tireSize':
+        this.setState({tireSize: value});
+      default:
+        return false;
+    }
   }
 }
---
+++ src/projects/bike/components/Cog.js
@@ -0,0 +1,115 @@
+import React, { Component } from 'react';
+
+export default class Cog extends Component {
+  static defaultProps = {
+    size: 64,
+    d1: 1,
+    d2: 0.95,
+    d3: 0.125,
+    teeth: 8,
+    splay: .375,
+    fill: 'currentcolor',
+  }
+
+  render() {
+    const { size, d1, d2, d3, teeth, splay, fill, className } = this.props;
+
+    // Center
+    const c = size / 2
+
+    // Radii
+    const r1 = d1 * size / 2
+    const r2 = d2 * size / 2
+    const r3 = d3 * size / 2
+
+    // Angle
+    const angle = 360 / teeth
+    const offset = 90
+
+    const viewBox = [0, 0, size, size].join(' ')
+
+    const rad = function(a) {
+      return Math.PI * a / 180
+    }
+
+    const rx = function(r, a) {
+      return c + r * Math.cos(rad(a))
+    }
+
+    const ry = function(r, a) {
+      return c + r * Math.sin(rad(a))
+    }
+
+    const num = function(n) {
+      return (n < 0.0000001) ? 0 : n
+    }
+
+    // Angle offsets to splay tooth
+    const ta = angle / 4
+    const splayTa = splay * ta
+    const tw = Math.tan(rad(ta - splayTa)) * r1
+
+    // Flat tooth end x and y coordinates
+    const tx = function(a, w) {
+      return Math.sin(rad(a)) * w
+    }
+
+    const ty = function(a, w) {
+      return Math.cos(rad(a)) * w
+    }
+
+    const drawTeeth = function(n) {
+      const d = [];
+
+      for (let i = 0; i < n; i++) {
+        const a = angle * i - offset
+        const a1 = a + ta + splayTa
+        const a2 = a + angle - ta - splayTa
+        const line = [
+          (i === 0) ? 'M' : 'L',
+          num(rx(r1, a) + tx(a, tw)),
+          num(ry(r1, a) - ty(a, tw)),
+          'L',
+          num(rx(r1, a) - tx(a, tw)),
+          num(ry(r1, a) + ty(a, tw)),
+          'L',
+          num(rx(r2, a1)),
+          num(ry(r2, a1)),
+          'A', r2, r2,
+          '0 0 1',
+          num(rx(r2, a2)),
+          num(ry(r2, a2)),
+        ].join(' ')
+        d.push(line)
+      }
+
+      return d.join(' ');
+    }
+
+    const hole = function() {
+      return [
+        'M', c, c - r3,
+        'A', r3, r3,
+        '0 0 0',
+        c, c + r3,
+        'A', r3, r3,
+        '0 0 0',
+        c, c - r3,
+      ].join(' ');
+    };
+
+    const pathData = [drawTeeth(teeth), hole()].join(' ');
+
+    return (
+      <svg
+        xmlns="http://www.w3.org/svg/2000"
+        viewBox={viewBox}
+        width={size}
+        height={size}
+        fill={fill}
+        className={className}>
+        <path d={pathData} />
+      </svg>
+    );
+  }
+}
